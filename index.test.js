3661847642c4babb98270872510893bb996b8e8e
Luis Herranz <luisherranz@gmail.com>
2018-06-20 11:34:24 +0200

    test(index): for new string feature

--- index.test.js
+++ index.test.js
@@ -1,3 +1,9 @@
+import { newFeature } from './index';
+
 test('should pass', () => {
   expect(3).toBe(3);
+});
+
+test('new Feature text', () => {
+  expect(newFeature).toBe('sooo important new feature');
 });
